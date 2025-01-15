b0a8f257824d7117a4f811a073219e93cc9f2a35
William Otto <william@wannacode.com>
2025-01-15 16:35:51 -0300

    Clean unused files

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Intro } from "@/components";
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Intro />
+    </View>
+  );
+}
---
+++ components/Intro.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export const Intro = () => {
+  return (
+    <View>
+      <Text>Introduction</Text>
+    </View>
+  );
+};
